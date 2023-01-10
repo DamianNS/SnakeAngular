8c13711ee9c8dedbdddc218996b01eb0c4564242
Damián Saint Paul <damian@niquelsoft.com>
2023-01-10 20:01:37 -0300

    environment.urlBack

--- src/app/servicios/backend.service.ts
+++ src/app/servicios/backend.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
+  private urlBack = environment.urlBack;
+
   constructor(
     private http: HttpClient
   ) { }
 
   crearServer(name:string): Observable<string> {
-    return this.http.get<string>(`https://localhost:7064/api/server/start/${name}`);
+    return this.http.get<string>(`${this.urlBack}/api/server/start/${name}`);
   }
 }
